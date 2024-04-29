4b45460df07de57df47e4fe24dafc66014c79725
Miles Lemi <mileslemi@gmail.com>
2024-04-29 18:20:14 +0300

    _set up frontend react. Installed packages. Set up routes

--- authfronted/src/App.js
+++ authfronted/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./hocs/Layout";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import ResetPassword from "./containers/ResetPassword";
+import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+import Activate from "./containers/Activate";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/login" Component={Login} />
+          <Route exact path="/signup" Component={Signup} />
+          <Route exact path="/reset_password" Component={ResetPassword} />
+          <Route
+            exact
+            path="/password/reset/confirm/:uid/:token"
+            Component={ResetPasswordConfirm}
+          />
+          <Route exact path="/activate/:uid/:token" Component={Activate} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
